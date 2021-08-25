ffc412545232243f2dd12d04c30045476a6e5e09
Emerson <eemr3@yahoo.com.br>
2021-08-25 18:50:59 -0300

    Corrigindo bug em exercício nota aluno

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica-exercicio/nota-aluno.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica-exercicio/nota-aluno.js
@@ -1,15 +1,17 @@
-let noataDoAluno = 80.5;
+let noataDoAluno = 105;
 
-if (noataDoAluno >= 90) {
+if (noataDoAluno >= 90 && noataDoAluno <= 100) {
   console.log(`Sua nota foi A`);
-} else if (noataDoAluno >= 80) {
+} else if (noataDoAluno >= 80 && noataDoAluno < 90) {
   console.log(`Sua nota foi B`);
-} else if (noataDoAluno >= 70) {
+} else if (noataDoAluno >= 70 && noataDoAluno < 80) {
   console.log(`Sua nota foi C`);
-} else if (noataDoAluno >= 60) {
+} else if (noataDoAluno >= 60 && noataDoAluno < 70) {
   console.log(`Sua nota foi D`);
-} else if (noataDoAluno >= 50) {
+} else if (noataDoAluno >= 50 && noataDoAluno < 60) {
   console.log(`Sua nota foi E`);
-} else {
+} else if (noataDoAluno < 50 && noataDoAluno >= 0) {
   console.log(`Sua nota foi F`);
+} else {
+  console.log("Nota inválida!");
 }
