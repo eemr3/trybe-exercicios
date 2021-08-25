f098f7641c0a92435f562866c67750d766707309
Emerson <eemr3@yahoo.com.br>
2021-08-25 15:04:24 -0300

    Exercício conteúdo: Operadores lógicos e switch case

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/conteudo/switch-case.js
@@ -0,0 +1,16 @@
+let estadoAprovacaoAluno = "Lista";
+
+switch (estadoAprovacaoAluno) {
+  case "Aprovada":
+    console.log("Parabéns, você foi aprovada(o)!");
+    break;
+  case "Lista":
+    console.log("Você está na nossa lista de espera");
+    break;
+  case "Reprovada":
+    console.log("Você foi reprovada(o)");
+    break;
+  default:
+    console.log("Não se aplica!");
+    break;
+}
