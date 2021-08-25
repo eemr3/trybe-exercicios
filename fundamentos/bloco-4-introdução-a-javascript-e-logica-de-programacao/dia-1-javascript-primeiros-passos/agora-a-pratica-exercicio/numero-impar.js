23f48f9003f6d49b59945aea9a7d01924488a4f6
Emerson <eemr3@yahoo.com.br>
2021-08-25 19:01:07 -0300

    Exercício numero ímpar

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica-exercicio/numero-impar.js
@@ -0,0 +1,9 @@
+const numberOne = 2;
+const numberTwo = 4;
+const numetThree = 50;
+
+if (numberOne % 2 === 1 || numberTwo % 2 === 1 || numetThree % 2 === 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
