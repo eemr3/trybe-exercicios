a2521ea396249f1122affe563ed8f3aa7817e840
Emerson <eemr3@yahoo.com.br>
2021-08-25 18:56:32 -0300

    Exercício numero par

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica-exercicio/numero-par.js
@@ -0,0 +1,9 @@
+const numberOne = 2;
+const numberTwo = 3;
+const numetThree = 5;
+
+if (numberOne % 2 === 0 || numberTwo % 2 === 0 || numetThree % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
