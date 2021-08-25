e044b197275ee3dc8c8ec6bdce576e22b504e8d7
Emerson <eemr3@yahoo.com.br>
2021-08-25 18:44:23 -0300

    Exercício nota aluno

---
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/agora-a-pratica-exercicio/nota-aluno.js
@@ -0,0 +1,15 @@
+let noataDoAluno = 80.5;
+
+if (noataDoAluno >= 90) {
+  console.log(`Sua nota foi A`);
+} else if (noataDoAluno >= 80) {
+  console.log(`Sua nota foi B`);
+} else if (noataDoAluno >= 70) {
+  console.log(`Sua nota foi C`);
+} else if (noataDoAluno >= 60) {
+  console.log(`Sua nota foi D`);
+} else if (noataDoAluno >= 50) {
+  console.log(`Sua nota foi E`);
+} else {
+  console.log(`Sua nota foi F`);
+}
