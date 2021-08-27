acf8213f4609ed49dffdcb2ff190968d0f1a8f7f
Emerson <eemr3@yahoo.com.br>
2021-08-27 20:01:05 -0300

    Exercício - vamos praticar: Terceiro exercicio

--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/primeiro-exercicio.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/primeiro-exercicio.js
@@ -3,7 +3,7 @@ const n = 5;
 for (let index = 1; index <= n; index += 1) {
   let first = "*";
   for (let secondIndex = 1; secondIndex <= n - 1; secondIndex += 1) {
-    first += "*";
+    first = "*";
   }
   console.log(first);
 }
--- fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/segundo-exercicio.js
+++ fundamentos/bloco-4-introdução-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/segundo-exercicio.js
@@ -1,9 +1,18 @@
 const n = 5;
 
 for (let index = 1; index <= n; index += 1) {
-  let asterisk = "*";
-  for (let secondIndex = 1; secondIndex <= index - 1; secondIndex += 1) {
+  let asterisk = "";
+  for (let secondIndex = 1; secondIndex <= index; secondIndex += 1) {
     asterisk += "*";
   }
   console.log(asterisk);
 }
+
+// let n = 5;
+
+// let resultado = "";
+
+// for (let index = 0; index < n; index += 1) {
+//   resultado += "*";
+//   console.log(resultado);
+// }
