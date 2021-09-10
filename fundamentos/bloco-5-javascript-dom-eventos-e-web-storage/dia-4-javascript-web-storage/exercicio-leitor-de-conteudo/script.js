95a1ed791aa96b66dd4a12988683d8716fba782e
Emerson <eemr3@yahoo.com.br>
2021-09-09 23:03:20 -0300

    Adiciona funcionalidade aterar tamanho da fonte

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicio-leitor-de-conteudo/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicio-leitor-de-conteudo/script.js
@@ -18,3 +18,26 @@ function setColorStorage() {
   return colorFontValeu;
 }
 setColorStorage();
+
+function sizeFont() {
+  const sizeFont = document.querySelector("#size-font");
+
+  sizeFont.addEventListener("input", function () {
+    webStorageValues.setSizeFontStorage("fontSize", sizeFont.value);
+    setSizeFontStorage();
+  });
+  let valueSize = setSizeFontStorage();
+  sizeFont.value = valueSize;
+}
+sizeFont();
+
+function setSizeFontStorage() {
+  const sizeFont = localStorage.getItem("fontSize");
+  const paragraph = document.querySelectorAll("p");
+
+  for (let index = 0; index < paragraph.length; index += 1) {
+    paragraph[index].style.fontSize = `${sizeFont}px`;
+  }
+  return sizeFont;
+}
+setSizeFontStorage();
