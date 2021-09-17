fe42fa70420c52c70ea19a50cf328ed4eba4036b
Emerson <eemr3@yahoo.com.br>
2021-09-17 12:57:35 -0300

    Implementa lógica para o popup-validation

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -57,7 +57,9 @@ const uf = [
   "SP",
   "TO",
 ];
+
 validation.init("#form-trybe");
+
 // validation.highlight();
 // function validateDataForm() {
 //   const inputsForm = document.querySelectorAll(".input-form");
@@ -138,142 +140,61 @@ function setPikedayLibDate() {
 
 setPikedayLibDate();
 
-// function getDateAndValidate() {
-//   const dateInput = document.querySelector("#datepicker");
-//   dateInput.DatePickerX.init({
-//     mondayFirst: true,
-//     format: "dd/mm/yyyy",
-//     minDate: new Date(0, 0),
-//     maxDate: new Date(9999, 11, 31),
-//     weekDayLabels: ["Mo", "Tu", "We", "Th", "Fr", "St", "Su"],
-//     shortMonthLabels: [
-//       "Jan",
-//       "Feb",
-//       "Mar",
-//       "Apr",
-//       "May",
-//       "Jun",
-//       "Jul",
-//       "Aug",
-//       "Sep",
-//       "Oct",
-//       "Nov",
-//       "Dec",
-//     ],
-//     singleMonthLabels: [
-//       "January",
-//       "February",
-//       "March",
-//       "April",
-//       "May",
-//       "June",
-//       "July",
-//       "August",
-//       "September",
-//       "October",
-//       "November",
-//       "December",
-//     ],
-//     todayButton: true,
-//     todayButtonLabel: "Today",
-//     clearButton: true,
-//     clearButtonLabel: "Clear",
-//   });
-//   const spanErroData = document.querySelector("#erro-data");
-//   const dataFormat = dateInput.value.replace(/-/g, "/");
-//   const validateDia = dataFormat.split("/")[0];
-//   const validaMes = dataFormat.split("/")[1];
-//   const validaAno = dataFormat.split("/")[2];
-//   const message = "";
-//   if (!validateDia > 0 || validateDia > 31) {
-//     return (spanErroData.innerText =
-//       "Dia do mês incorreto! Não pode ser menor que 0 e nem mairo 31.");
-//   }
-//   if (!validaMes > 0 || validaMes > 12) {
-//     return (spanErroData.innerText =
-//       "Mês do ano incorreto! O mês não pode ser menor que 0 e nem mairo que 12");
-//   }
-//   if (!validaAno > 0) {
-//     return (spanErroData.innerText =
-//       "Ano incorreto! O ano não pode ser menor que 0");
-//   }
-//   return true;
-// }
+function getValueRadioButton() {
+  const complento = document.querySelectorAll(".form-check-input");
+  for (let index = 0; index < complento.length; index += 1) {
+    if (complento[index].checked) {
+      return complento[index].value;
+    }
+  }
+}
 
-// function getValueRadioButton() {
-//   const complento = document.querySelectorAll(".complemento-input");
-//   for (let index = 0; index < complento.length; index += 1) {
-//     if (complento[index].checked) {
-//       return complento[index].value;
-//     }
-//   }
-// }
+function setDadosDoCurriculo() {
+  const areaDeMotagemCurriculo = document.querySelector("#curriculo-pronto");
+  const nameInpur = document.querySelector("#name-input");
+  const emailInput = document.querySelector("#email-input");
+  const enderecoInput = document.querySelector("#endereco-input");
+  const cidadeImput = document.querySelector("#cidade-input");
+  const estadoInput = document.querySelector("#estado-select");
+  const complento = getValueRadioButton();
 
-// function setDadosDoCurriculo() {
-//   const areaDeMotagemCurriculo = document.querySelector("#curriculo-pronto");
-//   const nameInpur = document.querySelector("#name-input");
-//   const emailInput = document.querySelector("#email-input");
-//   const enderecoInput = document.querySelector("#endereco-input");
-//   const cidadeImput = document.querySelector("#cidade-input");
-//   const estadoInput = document.querySelector("#estado-select");
-//   const complento = getValueRadioButton();
+  const textAreaInput = document.querySelector("#curriculo-text");
+  const cargoInput = document.querySelector("#cargo-input");
+  const descricaoCargo = document.querySelector("#descricao-cargo");
+  const dataInicioInput = document.querySelector("#datepicker");
+  const arrayvalueElement = [
+    nameInpur.value,
+    emailInput.value,
+    enderecoInput.value,
+    cidadeImput.value,
+    estadoInput.value,
+    complento,
+    textAreaInput.value,
+    cargoInput.value,
+    descricaoCargo.value,
+    dataInicioInput.value,
+  ];
 
-//   const textAreaInput = document.querySelector("#curriculo-text");
-//   const cargoInput = document.querySelector("#cargo-input");
-//   const descricaoCargo = document.querySelector("#descricao-cargo");
-//   const dataInicioInput = document.querySelector("#datepicker");
-//   const arrayvalueElement = [
-//     nameInpur.value,
-//     emailInput.value,
-//     enderecoInput.value,
-//     cidadeImput.value,
-//     estadoInput.value,
-//     complento,
-//     textAreaInput.value,
-//     cargoInput.value,
-//     descricaoCargo.value,
-//     dataInicioInput.value.replace(/-/g, "/"),
-//   ];
+  for (let index = 0; index < 10; index += 1) {
+    const divRsultado = document.createElement("div");
+    divRsultado.innerHTML = arrayvalueElement[index];
+    areaDeMotagemCurriculo.appendChild(divRsultado);
+  }
+}
 
-//   for (let index = 0; index < 10; index += 1) {
-//     const divRsultado = document.createElement("div");
-//     divRsultado.innerHTML = arrayvalueElement[index];
-//     areaDeMotagemCurriculo.appendChild(divRsultado);
-//   }
-// }
+const btnEnviarForm = document.querySelector("#botao-enviar");
+btnEnviarForm.addEventListener("click", (event) => {
+  event.preventDefault();
 
-// const btnEnviarForm = document.querySelector("#botao-enviar");
-// btnEnviarForm.addEventListener("click", (event) => {
-//   event.preventDefault();
-//   const spanMessage = document.querySelectorAll(".erro-form");
-//   const spanErroTextArea = document.querySelector("#erro-textArea");
-//   const spanErroData = document.querySelector("#erro-data");
+  if (!validation.validate()) {
+    validation.highlight();
+  } else {
+    setDadosDoCurriculo();
+  }
+});
 
-//   const messageInput = validateDataForm();
-//   const messageTextArea = validaTextArea();
-//   const messageDateErro = getDateAndValidate();
-
-//   for (let index = 0; index < spanMessage.length; index += 1) {
-//     if (messageInput !== "") {
-//       spanMessage[index].innerHTML = messageInput;
-//     } else {
-//       spanMessage[index].innerText = "";
-//     }
-//   }
-
-//   if (messageTextArea !== "") {
-//     return (spanErroTextArea.innerText = messageTextArea);
-//   }
-
-//   if (messageDateErro === true) {
-//     setDadosDoCurriculo();
-//   } else {
-//     return (spanErroData.innerText = messageDateErro);
-//   }
-// });
-
-// const btnReset = document.querySelector("#limpar-dados");
-// btnReset.addEventListener("click", () => {
-//   const areaDeMotagemCurriculo = document.querySelector("#curriculo-pronto");
-//   areaDeMotagemCurriculo.innerHTML = "";
-// });
+const btnReset = document.querySelector("#limpar-dados");
+btnReset.addEventListener("click", () => {
+  const areaDeMotagemCurriculo = document.querySelector("#curriculo-pronto");
+  areaDeMotagemCurriculo.innerHTML = "";
+});
