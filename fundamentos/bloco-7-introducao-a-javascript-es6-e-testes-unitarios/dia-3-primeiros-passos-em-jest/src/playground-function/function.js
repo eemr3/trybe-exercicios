dc83dcaeb5fb9fce610c5d60a686c4f40251885c
Emerson <eemr3@yahoo.com.br>
2021-09-28 22:21:16 -0300

    Implementa testes requsito 4.2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/src/playground-function/function.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/src/playground-function/function.js
@@ -1,5 +1,13 @@
-const encode = () => {};
+const encode = (str) => {
+  return str
+    .replace(/a/g, 1)
+    .replace(/e/g, 2)
+    .replace(/i/g, 3)
+    .replace(/o/g, 4)
+    .replace(/u/g, 5);
+};
 
+// console.log(encode("carteira"));
 const decode = () => {};
 
 module.exports = {
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/src/playground-function/function.spec.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/src/playground-function/function.spec.js
@@ -8,4 +8,16 @@ describe("Requirement four", () => {
   it("check if decode is a function", () => {
     expect(typeof decode).toBe("function");
   });
+
+  it("check if vowels 'a, e, i, o, u' are converted to '1, 2, 3, 4 and 5'", () => {
+    expect(encode("carteira")).toBe("c1rt23r1");
+    expect(encode("universidade")).toBe("5n3v2rs3d1d2");
+    expect(encode("borboleta")).toBe("b4rb4l2t1");
+  });
+
+  it("numbers '1, 2, 3, 4 and 5' are converted into vowels 'a, e, i, o, u'", () => {
+    expect(decode("c1rt23r1")).toBe("carteira");
+    expect(decode("5n3v2rs3d1d2")).toBe("universidade");
+    expect(decode("b4rb4l2t1")).toBe("borboleta");
+  });
 });
