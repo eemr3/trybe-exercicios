50e367160679516a126165cc88b13b05e2c9ebff
Emerson <eemr3@yahoo.com.br>
2021-10-04 18:30:32 -0300

    Implmenta código requisito 3 - exercício bonus

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/vamos-praticar-exercicio/requisito-bonus.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/vamos-praticar-exercicio/requisito-bonus.js
@@ -42,4 +42,29 @@ const randomDamageWarriorValue = (strength, weaponDmg) => {
   return damegeWarrior;
 };
 
+/* 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0. */
+const damageAndManaSpent = (intelligence, mana, turn) => {
+  let manaConsumed = 0;
+  if (mana === 0) {
+    manaConsumed += 0;
+    return 'Não possui mana suficiente'
+  } else {
+    manaConsumed += 15 * turn
+  }
+  console.log(manaConsumed)
+
+  const mageObject = {
+    dano: Math.floor(
+      Math.random() * (intelligence * 2 - intelligence) + intelligence
+    ),
+    mana: mana - manaConsumed,
+  };
+
+  return mageObject
+}
+
+
+
 
