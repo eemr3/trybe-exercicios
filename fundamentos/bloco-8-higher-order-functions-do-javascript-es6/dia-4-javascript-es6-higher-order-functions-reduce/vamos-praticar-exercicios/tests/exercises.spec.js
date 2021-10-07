1736df98ad9da354c6b93762c481448692e92028
Emerson <eemr3@yahoo.com.br>
2021-10-07 20:33:42 -0300

    Implementa código do teste e função requisito 2 - exercício dia 8.4

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/vamos-praticar-exercicios/tests/exercises.spec.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/vamos-praticar-exercicios/tests/exercises.spec.js
@@ -1,8 +1,15 @@
 const flatten = require('../src/exercise1');
+const reduceNames = require("../src/exercise2");
 
 describe('trabalhando com reduce', () => {
   it('retorne um array aprtir de uma matriz', () => {
     const expectedResultEx1 = ["1", "2", "3", true, 4, 5, 6];
     expect(flatten()).toEqual(expectedResultEx1);
   });
+
+  it('retorne uma string apartir de um array de objetos', () => {
+    const expectedResultEx2 =
+      "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+    expect(reduceNames()).toBe(expectedResultEx2);
+  })
 });
