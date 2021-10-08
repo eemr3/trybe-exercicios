8fc01fd6e5fec80c0b8b844042b2af2b1498ff05
Emerson <eemr3@yahoo.com.br>
2021-10-08 18:57:37 -0300

    Implementa código do teste e função requisito 3 - exercício dia 8.5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/vamos-praticar-exercicios/tests/exercises.spec.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/vamos-praticar-exercicios/tests/exercises.spec.js
@@ -1,10 +1,8 @@
 const rectangleArea = require('../src/exercise1');
 const sum = require('../src/exercise2');
+const personLikes = require("../src/exercise3");
 
 describe('trabalhando do spreed, rest, parametre defalut', () => {
-  const rectangle1 = [1, 2];
-  const rectangle2 = [3, 5];
-
   it('retorne a área dos 3 retângulos', () => {
     expect(rectangleArea(1,2, 3,5)).toBe(2, 15, 54);
   });
@@ -14,5 +12,28 @@ describe('trabalhando do spreed, rest, parametre defalut', () => {
     expect(sum(1,2,3)).toBe(6);
     expect(sum(1,2,3,4)).toBe(10);
     expect(sum(1,2,3,4,5)).toBe(15);
+  });
+
+  it('retone uma string contendo dados dos objetos passados como parametro', () => {
+    const alex = {
+      name: "Alex",
+      age: 26,
+      likes: ["fly fishing"],
+      nationality: "Australian",
+    };
+
+    const gunnar = {
+      name: "Gunnar",
+      age: 30,
+      likes: ["hiking", "scuba diving", "taking pictures"],
+      nationality: "Icelandic",
+    };
+
+    expect(personLikes(alex)).toBe("Alex is 26 years old and likes fly fishing.");
+    expect(
+      personLikes(gunnar)
+    ).toBe(
+      "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures."
+    );
   });
 });
