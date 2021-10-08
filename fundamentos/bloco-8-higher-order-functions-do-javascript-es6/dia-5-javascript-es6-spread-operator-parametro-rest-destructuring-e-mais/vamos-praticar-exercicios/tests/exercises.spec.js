baec2e99818d823f522e6e336725c735fc4cbcda
Emerson <eemr3@yahoo.com.br>
2021-10-08 18:44:27 -0300

    Implementa código do teste e função requisito 2 - exercício dia 8.5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/vamos-praticar-exercicios/tests/exercises.spec.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/vamos-praticar-exercicios/tests/exercises.spec.js
@@ -1,9 +1,18 @@
 const rectangleArea = require('../src/exercise1');
+const sum = require('../src/exercise2');
 
 describe('trabalhando do spreed, rest, parametre defalut', () => {
   const rectangle1 = [1, 2];
   const rectangle2 = [3, 5];
+
   it('retorne a área dos 3 retângulos', () => {
     expect(rectangleArea(1,2, 3,5)).toBe(2, 15, 54);
+  });
+
+  it('retorne a soma de valores passados como paramentro', () => {
+    expect(sum(1,2)).toBe(3);
+    expect(sum(1,2,3)).toBe(6);
+    expect(sum(1,2,3,4)).toBe(10);
+    expect(sum(1,2,3,4,5)).toBe(15);
   });
 });
