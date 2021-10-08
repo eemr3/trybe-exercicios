d5ff63594454a92631a2950ec02522d1e853172e
Emerson <eemr3@yahoo.com.br>
2021-10-08 15:22:56 -0300

    Implementa códigio conteúdo dia 8.5 rest operator

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/conteudo-exemplo/exemplo.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/conteudo-exemplo/exemplo.js
@@ -1,3 +1,5 @@
+/* Spreed operator */
+
 // const numbers = [1, 2, 3];
 
 // const newArray = [...numbers, 4, 5, 6];
@@ -34,4 +36,16 @@ const about = {
 };
 
 const customer = { ...people, ...about };
-console.log(customer); 
+// console.log(customer); 
+
+/* Rest operator */
+function quantosParams(...args) {
+  console.log('parâmetros:', args);
+  return `Você passou ${args.length} parâmetros para a função.`;
+}
+
+// console.log(quantosParams(0, 1, 2)); // Você passou 3 parâmetros para a função.
+// console.log(quantosParams('string', null, [1, 2, 3], { })); // Você passou 4 parâmetros para a função.
+
+const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
+console.log(sum(4, 7, 8, 9, 60)); 
