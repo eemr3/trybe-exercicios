6726523fe63dd4a462ec5a2866f6c86dd2acbfd0
Emerson <eemr3@yahoo.com.br>
2021-10-19 13:44:31 -0300

    Exercícios do dia 9.2 - finalizado

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/vamos-praticar-exercicios/coincap-crypto/api.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/vamos-praticar-exercicios/coincap-crypto/api.js
@@ -1,14 +1,19 @@
 const URL_API = "https://api.coincap.io/v2/assets";
+const API_URL ={
+  baseUrl: `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest`,
+  endpoint: `/currencies/usd.min.json`
+}
+const resultConvert = [];
 
 function printResponseAPI(data) {
   const ulList = document.querySelector("#container-list");
-  
+  const valueConvert = resultConvert[0].toFixed(2);
+  console.log(valueConvert)
   data.reduce((acc, coin) => {
     if(acc !== 0){
+      const resutlCoin = Number(coin.priceUsd).toFixed(2) * valueConvert
       const listName = document.createElement("p");
-      listName.innerHTML = `${coin.name}(${coin.symbol}): ${Number(
-        coin.priceUsd
-      ).toFixed(2)}`;
+      listName.innerHTML = `${coin.name} (${coin.symbol}): R$ ${resutlCoin.toFixed(2)}`;
 
       ulList.appendChild(listName);
       acc -= 1;
@@ -16,6 +21,18 @@ function printResponseAPI(data) {
     return acc;
 
   }, 10)
+}
+
+
+const brlCovertCrypto = async () => {
+  
+  const response = await fetch(`${API_URL.baseUrl}${API_URL.endpoint}`
+  );
+  const data = await response.json(); 
+
+  resultConvert.push(data.usd.brl);
+  
+  
 }
 
 const fetchCoincap = async () => {
@@ -28,4 +45,5 @@ const fetchCoincap = async () => {
   }
 };
 
-fetchCoincap();
+fetchCoincap(); 
+ brlCovertCrypto()
