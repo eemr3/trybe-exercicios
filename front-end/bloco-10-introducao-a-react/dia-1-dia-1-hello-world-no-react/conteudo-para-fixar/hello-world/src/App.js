9508748909dd8ea8cae43a97794f2ddbfd1ddd27
Emerson <eemr3@yahoo.com.br>
2021-10-25 15:33:58 -0300

    Dia 1 - Front-End - Conteúdo para fixar Exercício

---
+++ front-end/bloco-10-introducao-a-react/dia-1-dia-1-hello-world-no-react/conteudo-para-fixar/hello-world/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import About from './Component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Front-End: Dia 1 - Introdução ao React.JS</h1>
+      </header>
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/bloco-10-introducao-a-react/dia-1-dia-1-hello-world-no-react/conteudo-para-fixar/hello-world/src/Component.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Emerson Moriera</h1>
+        <p>Tenho 45 anos, casado e pai de 2 filhos! Estudante de Web Develope na Trybe.</p>
+      </>
+    );
+  }
+}
+export default About;
