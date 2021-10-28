f0390a6e802ed5573ab199a275b367a1934284e9
Emerson <eemr3@yahoo.com.br>
2021-10-28 15:27:12 -0300

    Exemplo 4 do conteúdo para fixar - melhorias

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/conteudo-para-fixar/user-profile/src/components/UserProfile.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/conteudo-para-fixar/user-profile/src/components/UserProfile.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Image from './Image';
 
+import './UserProfile.css';
+
 class UserProfile extends Component {
   render() {
+    const { avatar, name, email } = this.props.user;
     return (
-      <div>
-        <p>{this.props.user.name}</p>
-        <p>{this.props.user.email}</p>
-        <Image source={this.props.user.avatar} alternativeText='User avatar' />
+      <div className='container'>
+        <Image source={avatar} alternativeText='User avatar' />
+        <p>{name}</p>
+        <p>{email}</p>
       </div>
     );
   }
