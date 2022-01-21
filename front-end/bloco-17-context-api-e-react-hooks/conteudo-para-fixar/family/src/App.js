05886b9ef4244d1576668d23a3dcfe6f2b223df0
Emerson <eemr3@yahoo.com.br>
2022-01-21 10:58:59 -0300

    exercicios

---
+++ front-end/bloco-17-context-api-e-react-hooks/conteudo-para-fixar/family/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import GreatGrandfather from './GreatGrandfather';
+
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GreatGrandfather />
+      </div>
+    );
+  }
+}
+
+export default App;
